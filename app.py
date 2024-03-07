a12ae49161ce63d5faa663e9f17a438e300d38be
88maurosls <162556108+88maurosls@users.noreply.github.com>
2024-03-07 16:14:21 +0100

    Update app.py

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ def main():
             # Applica la formattazione condizionale alle celle della colonna 'customer PO'
             result_df_styled = result_df.style.apply(highlight_customer_po, axis=0)
             # Visualizzazione della tabella con Streamlit
-            st.dataframe(result_df_styled, height=None, expand_frame=True)
+            st.write(result_df_styled)
         else:
             st.error("BARCODE NON TROVATO!!!!!!")
 
