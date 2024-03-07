2a60c6eff0a1bfb7790148ac718989a06b287e27
88maurosls <162556108+88maurosls@users.noreply.github.com>
2024-03-07 15:10:03 +0100

    Update app.py

--- app.py
+++ app.py
@@ -21,13 +21,16 @@ def main():
 
     df = df[['Collo', 'customer PO', 'SKU', 'Size', 'Unità', 'UPC', 'Made in', 'Import Date']]
 
-    # Utilizzo di un placeholder per resettare il widget
-    placeholder = st.empty()
+    # Check if 'clear_bar' key exists in session state, if not, initialize it.
+    if 'clear_bar' not in st.session_state:
+        st.session_state.clear_bar = False
 
-    # Aggiungo una key al widget e uso un bottone per resettarlo
-    bar = placeholder.text_input('Inserire il barcode', key='barcode_input')
+    # Create a text input that is tied to the 'clear_bar' state.
+    bar = st.text_input('Inserire il barcode', value="" if st.session_state.clear_bar else None, key='barcode_input')
 
+    # Check barcode against the dataframe.
     if st.button('Check'):
+        st.session_state.clear_bar = True  # Set the flag to clear the bar next time it's clicked on.
         result_df = df[df['Collo'] == bar]
         if not result_df.empty:
             st.success("Barcode TROVATO")
@@ -35,9 +38,10 @@ def main():
             st.dataframe(result_df_styled)
         else:
             st.error("BARCODE NON TROVATO!!!!!!")
-        
-        # Dopo aver cliccato, si elimina il testo dall'input
-        placeholder.empty()
+
+    # If the user interacts with the text input and 'clear_bar' is True, clear the text input.
+    if bar and st.session_state.clear_bar:
+        st.session_state.clear_bar = False  # Reset the flag as the bar has been cleared.
 
 if __name__ == "__main__":
     main()
