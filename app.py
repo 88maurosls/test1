d9b458b7f275259df316f70ed5dfd7fa75def357
88maurosls <162556108+88maurosls@users.noreply.github.com>
2024-03-07 12:01:45 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,28 +1,29 @@
 import streamlit as st
 import pandas as pd
 
-#https://docs.google.com/spreadsheets/d/1Ps6OqL1cLdCiD30VJTkDhSWKNYW2I7Uqhg1viCBvFXQ/edit
-
 def main():
     st.set_page_config(layout="wide")
     st.title("Frenz's Barcode App V2")
-    st.image('barcode.png')
 
     SHEET_ID = '1Ps6OqL1cLdCiD30VJTkDhSWKNYW2I7Uqhg1viCBvFXQ'
     SHEET_NAME = 'test'
-    foglio_id=0
-    url = f'https://docs.google.com/spreadsheets/d/{SHEET_ID}/gviz/tq?tqx=out:csv&sheet={SHEET_NAME}/edit#gid={foglio_id}'
-    df = pd.read_csv(url,dtype={'Collo':str})
-    bar = st.text_input('inserire il barcode')
-    #bar = "00008865511394193353" #testing
-    if st.button('check'):
-        if not df[df['Collo'] == bar].empty:
-            st.balloons()
-            st.success("Barcodes TROVATO:")
-            st.dataframe(df[df['Collo']==bar])
+    url = f'https://docs.google.com/spreadsheets/d/{SHEET_ID}/gviz/tq?tqx=out:csv&sheet={SHEET_NAME}'
+
+    # Tentativo di specificare una codifica e gestire valori nulli
+    df = pd.read_csv(url, dtype={'Collo': str, 'Customer PO': str}, encoding='utf-8', na_filter=False)
+
+    # Stampa dei dati grezzi per il debug
+    st.write(df.head())  # Stampa le prime cinque righe per il debug
+
+    bar = st.text_input('Inserire il barcode')
+
+    if st.button('Check'):
+        result = df[df['Collo'] == bar]
+        if not result.empty:
+            st.success("Barcode TROVATO:")
+            st.dataframe(result)
         else:
             st.error("BARCODE NON TROVATO!!!!!!")
-            #################################################
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
