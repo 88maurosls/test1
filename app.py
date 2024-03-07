2f44e898031c2a94662c03198eb636dc4587b27e
88maurosls <162556108+88maurosls@users.noreply.github.com>
2024-03-07 15:22:44 +0100

    Update app.py

--- app.py
+++ app.py
@@ -15,28 +15,23 @@ def main():
     SHEET_NAME = 'test'
     url = f'https://docs.google.com/spreadsheets/d/{SHEET_ID}/gviz/tq?tqx=out:csv&sheet={SHEET_NAME}'
 
-    dtype_dict = {'Collo': str}
+    dtype_dict = {'Collo': str}  
     converters = {'customer PO': str, 'UPC': str}
     df = pd.read_csv(url, dtype=dtype_dict, converters=converters)
 
     df = df[['Collo', 'customer PO', 'SKU', 'Size', 'Unità', 'UPC', 'Made in', 'Import Date']]
 
-    col1, col2 = st.columns((4, 1))  # Creating two columns for input and button.
-
-    with col1:
-        bar = st.text_input('Inserire il barcode', key='barcode_input')
-
-    with col2:
-        if st.button('Reset'):
-            # Clear the barcode input field.
-            st.session_state['barcode_input'] = ''
+    bar = st.text_input('Inserire il barcode')
 
     if st.button('Check'):
         result_df = df[df['Collo'] == bar]
         if not result_df.empty:
             st.success("Barcode TROVATO")
             result_df_styled = result_df.style.apply(highlight_customer_po, axis=0)
-            st.dataframe(result_df_styled)
+            if len(result_df) > 10:
+                st.dataframe(result_df_styled)
+            else:
+                st.dataframe(result_df_styled, height=None)
         else:
             st.error("BARCODE NON TROVATO!!!!!!")
 
