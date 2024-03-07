49bfd565451d50995e878a8d4a949ab25dc48691
88maurosls <162556108+88maurosls@users.noreply.github.com>
2024-03-07 14:58:12 +0100

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ def main():
     url = f'https://docs.google.com/spreadsheets/d/{SHEET_ID}/gviz/tq?tqx=out:csv&sheet={SHEET_NAME}'
 
     # Specifica manualmente il tipo di dati delle colonne durante il caricamento del CSV
-    dtype_dict = {'Collo': str}  
+    dtype_dict = {'Collo': str}
     # Utilizza la funzione `converters` per specificare il tipo di dati della colonna 'UPC' come `str`
     converters = {'customer PO': str, 'UPC': str}
     df = pd.read_csv(url, dtype=dtype_dict, converters=converters)
@@ -24,22 +24,23 @@ def main():
     # Ordina le colonne nel DataFrame
     df = df[['Collo', 'customer PO', 'SKU', 'Size', 'Unità', 'UPC', 'Made in', 'Import Date']]
 
-    # Casella di ricerca del barcode
-    bar = st.text_input('Inserire il barcode', key="barcode_input")
-
-    # Pulsante per svuotare la casella di ricerca
-    if st.button("Svuota casella di ricerca"):
-        st.experimental_rerun()
+    # Aggiungi una key univoca al widget text_input
+    bar = st.text_input('Inserire il barcode', key='barcode_input')
 
     if st.button('Check'):
         result_df = df[df['Collo'] == bar]
         if not result_df.empty:
             st.success("Barcode TROVATO")
+            # Applica la formattazione condizionale alle celle della colonna 'customer PO'
             result_df_styled = result_df.style.apply(highlight_customer_po, axis=0)
+            # Visualizzazione della tabella con Streamlit
             if len(result_df) > 10:
                 st.dataframe(result_df_styled)
             else:
                 st.dataframe(result_df_styled, height=None)
+
+            # Aggiorna il valore del widget text_input per cancellare il valore inserito
+            st.session_state.barcode_input = ""
         else:
             st.error("BARCODE NON TROVATO!!!!!!")
 
