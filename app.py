af0690d317563b7cfbd047c057dc270b65be1454
88maurosls <162556108+88maurosls@users.noreply.github.com>
2024-03-07 14:18:41 +0100

    Update app.py

--- app.py
+++ app.py
@@ -26,8 +26,8 @@ def main():
             st.success("Barcode TROVATO:")
             # Formattazione in grassetto per la colonna 'customer PO'
             result_df['customer PO'] = result_df['customer PO'].apply(lambda x: f"<b>{x}</b>")
-            # Creazione di una tabella HTML con intestazione fissa
-            table = f"<table><thead style='position: sticky; top: 0; background-color: white;'><tr>"
+            # Creazione di una tabella HTML con intestazione fissa e larghezza massima
+            table = f"<table style='width: 100%; max-width: 1000px;'><thead style='position: sticky; top: 0; background-color: white;'><tr>"
             for col in result_df.columns:
                 table += f"<th>{col}</th>"
             table += "</tr></thead><tbody>"
