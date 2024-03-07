7acbb558d3dd28076cc20b967a90258b2c1061e2
88maurosls <162556108+88maurosls@users.noreply.github.com>
2024-03-07 13:02:45 +0100

    Update app.py

--- app.py
+++ app.py
@@ -5,27 +5,23 @@ def main():
     st.set_page_config(layout="wide")
     st.title("Frenz's Barcode App V2")
 
-    # ID e nome del foglio del Google Sheet
     SHEET_ID = '1Ps6OqL1cLdCiD30VJTkDhSWKNYW2I7Uqhg1viCBvFXQ'
     SHEET_NAME = 'test'
     url = f'https://docs.google.com/spreadsheets/d/{SHEET_ID}/gviz/tq?tqx=out:csv&sheet={SHEET_NAME}'
 
-    # Lettura dei dati da Google Sheets
-    df = pd.read_csv(url)
+    # Tentativo di specificare una codifica e gestire valori nulli
+    df = pd.read_csv(url, dtype={'Collo': str, 'Customer PO': str}, encoding='utf-8', na_filter=False)
 
-    # Stampa i nomi delle colonne per assicurarti che siano corretti
-    st.write(df.columns)
-
-    # Assicurati di utilizzare il nome esatto della colonna come appare nel DataFrame
-    # Ad esempio, se il nome della colonna è 'customer PO' invece di 'Customer PO'
-    df['customer PO'] = df['customer PO'].astype(str)
+    # Stampa dei dati grezzi per il debug
+    st.write(df.head())  # Stampa le prime cinque righe per il debug
 
     bar = st.text_input('Inserire il barcode')
 
     if st.button('Check'):
-        if not df[df['Collo'] == bar].empty:
+        result = df[df['Collo'] == bar]
+        if not result.empty:
             st.success("Barcode TROVATO:")
-            st.dataframe(df[df['Collo'] == bar].reset_index(drop=True))
+            st.dataframe(result)
         else:
             st.error("BARCODE NON TROVATO!!!!!!")
 
