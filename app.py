7bbcab70d93c103e40d9ee5d702b0f37ebec4b10
88maurosls <162556108+88maurosls@users.noreply.github.com>
2024-03-07 13:55:30 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,23 @@
 import streamlit as st
 import pandas as pd
 
+def load_data(sheet_id, sheet_name):
+    url = f'https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv&sheet={sheet_name}'
+    # Specifica manualmente il tipo di dati delle colonne durante il caricamento del CSV
+    dtype_dict = {'Collo': str}  
+    # Utilizza la funzione `converters` per specificare il tipo di dati della colonna 'customer PO' come `object`
+    converters = {'customer PO': str}
+    df = pd.read_csv(url, dtype=dtype_dict, converters=converters)
+    return df
+
 def main():
     st.set_page_config(layout="wide")
     st.title("Frenz's Barcode App V2")
 
     SHEET_ID = '1Ps6OqL1cLdCiD30VJTkDhSWKNYW2I7Uqhg1viCBvFXQ'
     SHEET_NAME = 'test'
-    url = f'https://docs.google.com/spreadsheets/d/{SHEET_ID}/gviz/tq?tqx=out:csv&sheet={SHEET_NAME}'
 
-    # Specifica manualmente il tipo di dati delle colonne durante il caricamento del CSV
-    dtype_dict = {'Collo': str}  
-    # Utilizza la funzione `converters` per specificare il tipo di dati della colonna 'customer PO' come `object`
-    converters = {'customer PO': str}
-    df = pd.read_csv(url, dtype=dtype_dict, converters=converters)
+    df = load_data(SHEET_ID, SHEET_NAME)
 
     bar = st.text_input('Inserire il barcode')
 
