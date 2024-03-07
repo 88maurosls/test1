2cb613ad69d9da229a56daecc872f9bc10ff9712
88maurosls <162556108+88maurosls@users.noreply.github.com>
2024-03-07 14:17:01 +0100

    Update app.py

--- app.py
+++ app.py
@@ -26,7 +26,10 @@ def main():
             st.success("Barcode TROVATO:")
             # Formattazione in grassetto per la colonna 'customer PO'
             result_df['customer PO'] = result_df['customer PO'].apply(lambda x: f"<b>{x}</b>")
-            st.table(result_df, table_styles=[{'selector': 'thead', 'props': [('position', 'sticky'), ('top', '0')]}], unsafe_allow_html=True)
+            # Applicazione di stili CSS per l'intestazione della tabella
+            st.table(result_df.style.set_table_styles(
+                [{'selector': 'thead', 'props': [('position', 'sticky'), ('top', '0')]}]
+            ), unsafe_allow_html=True)
         else:
             st.error("BARCODE NON TROVATO!!!!!!")
 
