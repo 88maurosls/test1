c08819187cf7039d391d4ee4c57f45ac57f86d9d
88maurosls <162556108+88maurosls@users.noreply.github.com>
2024-03-07 15:45:21 +0100

    Update app.py

--- app.py
+++ app.py
@@ -24,10 +24,10 @@ def main():
     # Ordina le colonne nel DataFrame
     df = df[['Collo', 'customer PO', 'SKU', 'Size', 'Unità', 'UPC', 'Made in', 'Import Date']]
 
-    bar = st.text_input('Inserire il barcode')
+    bar1 = st.text_input('Inserire il barcode 1')
 
-    if st.button('Check'):
-        result_df = df[df['Collo'] == bar]
+    if st.button('Check 1'):
+        result_df = df[df['Collo'] == bar1]
         if not result_df.empty:
             st.success("Barcode TROVATO")
             # Applica la formattazione condizionale alle celle della colonna 'customer PO'
@@ -39,6 +39,24 @@ def main():
                 st.dataframe(result_df_styled, height=None)
         else:
             st.error("BARCODE NON TROVATO!!!!!!")
+
+    bar2 = st.text_input('Inserire il barcode 2')
+
+    if st.button('Check 2'):
+        result_df = df[df['Collo'] == bar2]
+        if not result_df.empty:
+            st.success("Barcode TROVATO")
+            # Applica la formattazione condizionale alle celle della colonna 'customer PO'
+            result_df_styled = result_df.style.apply(highlight_customer_po, axis=0)
+            # Visualizzazione della tabella con Streamlit
+            if len(result_df) > 10:
+                st.dataframe(result_df_styled)
+            else:
+                st.dataframe(result_df_styled, height=None)
+        else:
+            st.error("BARCODE NON TROVATO!!!!!!")
+            # Pulisci il contenuto della prima barra di ricerca
+            st.text_input('Inserire il barcode 1', value='')
 
 if __name__ == "__main__":
     main()
