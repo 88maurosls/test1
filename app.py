de4ce01415b476c75204e678a2d9bfc6ad351361
88maurosls <162556108+88maurosls@users.noreply.github.com>
2024-03-08 11:14:54 +0100

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,8 @@ def highlight_customer_po(value):
     else:
         return [''] * len(value)
 
-def check_barcode(barcode, df):
+def check_barcode():
+    barcode = st.session_state.barcode_input
     if barcode:
         st.write("Barcode cercato:", barcode)
         result_df = df[df['Collo'] == barcode]
@@ -27,7 +28,7 @@ def main():
     url = f'https://docs.google.com/spreadsheets/d/{SHEET_ID}/gviz/tq?tqx=out:csv&sheet={SHEET_NAME}'
 
     # Specifica manualmente il tipo di dati delle colonne durante il caricamento del CSV
-    dtype_dict = {'Collo': str}
+    dtype_dict = {'Collo': str}  
     # Utilizza la funzione `converters` per specificare il tipo di dati della colonna 'UPC' come `str`
     converters = {'customer PO': str, 'UPC': str}
     df = pd.read_csv(url, dtype=dtype_dict, converters=converters)
@@ -35,11 +36,15 @@ def main():
     # Ordina le colonne nel DataFrame
     df = df[['Collo', 'customer PO', 'SKU', 'Size', 'Unità', 'UPC', 'Made in', 'Import Date', 'Rif. Sped.']]
 
+    # Inizializzazione dello stato della sessione per il campo di input del barcode
+    if 'barcode_input' not in st.session_state:
+        st.session_state.barcode_input = ''
+
     # Barra di ricerca del barcode
-    barcode_input = st.text_input('Inserire il barcode', key='barcode_input', on_change=check_barcode, args=(st.session_state.barcode_input, df))
+    st.text_input('Inserire il barcode', key='barcode_input', on_change=check_barcode)
 
     if st.button('Check'):
-        check_barcode(st.session_state.barcode_input, df)
+        check_barcode()
 
 if __name__ == "__main__":
     main()
