58e458af42862ba7a467a4368c338d2387025bca
88maurosls <162556108+88maurosls@users.noreply.github.com>
2024-03-08 11:11:37 +0100

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,17 @@ def highlight_customer_po(value):
     else:
         return [''] * len(value)
 
+def check_barcode(barcode, df):
+    if barcode:
+        st.write("Barcode cercato:", barcode)
+        result_df = df[df['Collo'] == barcode]
+        if not result_df.empty:
+            st.success("TROVATA CORRISPONDENZA")
+            result_df_styled = result_df.style.apply(highlight_customer_po, axis=0)
+            st.table(result_df_styled)
+        else:
+            st.error("CORRISPONDENZA NON TROVATA")
+
 def main():
     st.set_page_config(layout="wide", initial_sidebar_state="collapsed")
     st.title("Dope Barcode Scanner v2.4")
@@ -16,7 +27,7 @@ def main():
     url = f'https://docs.google.com/spreadsheets/d/{SHEET_ID}/gviz/tq?tqx=out:csv&sheet={SHEET_NAME}'
 
     # Specifica manualmente il tipo di dati delle colonne durante il caricamento del CSV
-    dtype_dict = {'Collo': str}  
+    dtype_dict = {'Collo': str}
     # Utilizza la funzione `converters` per specificare il tipo di dati della colonna 'UPC' come `str`
     converters = {'customer PO': str, 'UPC': str}
     df = pd.read_csv(url, dtype=dtype_dict, converters=converters)
@@ -24,47 +35,11 @@ def main():
     # Ordina le colonne nel DataFrame
     df = df[['Collo', 'customer PO', 'SKU', 'Size', 'Unità', 'UPC', 'Made in', 'Import Date', 'Rif. Sped.']]
 
-    # Verifica se la chiave 'barcode_input' è presente in session_state, se non lo è, la inizializza a una stringa vuota
-    if "barcode_input" not in st.session_state:
-        st.session_state.barcode_input = ""
-
     # Barra di ricerca del barcode
-    if 'barcode_input' not in st.session_state:
-        st.session_state.barcode_input = ''
-    def submit():
-        st.session_state.barcode_input = st.session_state.widget
-        st.session_state.widget = ''
-    
-    st.text_input('Inserire il barcode', key='widget', on_change=submit)
-
-    bar = st.session_state.barcode_input
+    barcode_input = st.text_input('Inserire il barcode', key='barcode_input', on_change=check_barcode, args=(st.session_state.barcode_input, df))
 
     if st.button('Check'):
-        st.write("Barcode cercato:", bar)  # Visualizza il valore inserito nella barra di ricerca
-        result_df = df[df['Collo'] == bar]
-        if not result_df.empty:
-            st.success("TROVATA CORRISPONDENZA")
-            # Applica la formattazione condizionale alle celle della colonna 'customer PO'
-            result_df_styled = result_df.style.apply(highlight_customer_po, axis=0)
-            # Visualizzazione della tabella con Streamlit
-            st.table(result_df_styled)
-        else:
-            st.error("CORRISPONDENZA NON TROVATA")
-
-    # Incorpora il codice JavaScript utilizzando st.write
-    st.write(
-        """
-        <script>
-        document.addEventListener("DOMContentLoaded", function(event) {
-            document.addEventListener("keydown", function(event) {
-                if (event.key === "Enter") {
-                    document.querySelector(".stButton button").click();
-                }
-            });
-        });
-        </script>
-        """
-    )
+        check_barcode(st.session_state.barcode_input, df)
 
 if __name__ == "__main__":
     main()
